fcc07eeb8b88e22d0974b517190086ff5c2464a3
heytitle <pat.chormai@gmail.com>
2019-08-13 09:46:22 +0200

    fix installation

--- setup.py
+++ setup.py
@@ -8,21 +8,22 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="databuri",
-    package="databuri",
     version="0.0.1",
+    packages=setuptools.find_packages(),
     author="Pattarawat Chormai and friends",
     author_email="author@example.com",
     install_requires=dependencies,
-    # @todo #1 ...
+    # @todo #1 add description
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/codeforthailand/databuri",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         # @todo #1 add license
         # "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    # @todo #1 add keywords
+    # keywords=[...]
 )
